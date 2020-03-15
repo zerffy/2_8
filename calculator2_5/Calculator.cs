9d998977f2b37ff3edf40a7fa56ac8c1429ac731
bluem <bluemrl@163.com>
2020-03-15 13:39:02 +0800

    first commit

---
+++ calculator2_5/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator2_5
+{
+    class Calculator
+    {
+        float x;
+        float y;
+
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+        public void Add()
+        {
+            try
+            {
+                Console.WriteLine(x + "+" + y + "=" + (x + y));
+            }
+            catch
+            {
+                Console.WriteLine("无法运算！！！");
+            }
+            
+        }
+        public void Subtract()
+        {
+            try
+            {
+                Console.WriteLine(x + "-" + y + "=" + (x - y));
+            }
+            catch
+            {
+                Console.WriteLine("无法运算！！！");
+                
+            }
+        }
+        public void Multiplication()
+        {
+            try
+            {
+                Console.WriteLine(x + "*" + y + "=" + (x * y));
+            }
+            catch
+            {
+                Console.WriteLine("无法运算！！！");
+                
+            }
+        }
+        public void Division()
+        {
+            try
+            {
+                Console.WriteLine(x + "/" + y + "=" + (x / y));
+            }
+            catch
+            {
+                Console.WriteLine("无法运算！！！");
+                
+            }
+        }
+        public void Equals()
+        {
+            if (x == y)
+                Console.WriteLine("这两个整数相等");
+            else Console.WriteLine("这两个整数不相等");
+        }
+    }
+}
---
+++ calculator2_5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace calculator2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            try
+            {
+                string s = Console.ReadLine();//获取第一个数字
+                cal.X = Convert.ToInt32(s); //转换
+            }
+            catch
+            {
+                Console.WriteLine("输入第一个数字错误引发异常！！！");
+            }
+            string ysf= Console.ReadLine();//获取运算符
+            try
+            {
+                string t = Console.ReadLine();//获取第二个数字
+                cal.Y = Convert.ToInt32(t); //转换
+            }
+            catch
+            {
+                Console.WriteLine("输入第二个数字错误引发异常！！！");
+                Thread.Sleep(5000);
+            }
+            cal.Equals();
+            switch (ysf)
+            {
+                case "+":
+                    cal.Add();
+                    break;
+                case "-":
+                    cal.Subtract();
+                    break;
+                case "*":
+                    cal.Multiplication();
+                    break;
+                case "/":
+                    cal.Division();
+                    break;
+                default:
+                    Console.WriteLine("输入运算符错误引发异常!!!");
+                    break;
+            }
+        }
+    }
+}
